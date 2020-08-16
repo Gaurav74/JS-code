a2e44ac8125743b6687f9b41a847a499b2711ad9
Gaurav Kumar <2874georgian@gmail.com>
2020-08-16 19:10:07 +0530

    added sorting _array.js

---
+++ practice/sorting_array.js
@@ -0,0 +1,13 @@
+let array = [5, 4, 65, 86, 68, 62, 56, 6];
+console.log(" This is before sorting..... ");
+console.log(array);
+// while sorting array JS takes numeric as strings and sort them like string.
+// so it has to converted to number by passing a function.
+/*
+passing a function (a,b) return a-b means 
+if < 0 then a will come first
+if =0 then it will do nothing
+if > 0 then  b will come first. 
+*/
+array.sort((a, b) => { return a - a });
+console.log(array);
